fc2a91e1087708938c5441c1d8542267262fe1f6
Andrew Eisenberg <aeisenberg@vmware.com>
2011-07-28 15:14:01 -0700

    Fixing tests, clean up plugin.xml, updating target platform

--- org.maven.ide.eclipse.ajdt.tests/src/org/maven/ide/eclipse/ajdt/tests/AjdtProjectConfiguratorTest.java
+++ org.maven.ide.eclipse.ajdt.tests/src/org/maven/ide/eclipse/ajdt/tests/AjdtProjectConfiguratorTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.ajdt.core.AspectJCorePreferences;
 import org.eclipse.ajdt.core.AspectJPlugin;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -36,23 +37,26 @@ public class AjdtProjectConfiguratorTest extends AbstractMavenProjectTestCase {
     assertTrue("Expected AJDT nature", project.hasNature(AspectJPlugin.ID_NATURE));
 
     IJavaProject javaProject = JavaCore.create(project);
-    //List<IClasspathEntry> sources = getSources(javaProject.getRawClasspath());
-    //assertEquals(1, sources.size());
-    //assertEquals(project.getFolder("src/main/aspect").getFullPath(), sources.get(0).getPath());
+    List<IClasspathEntry> sources = getSources(javaProject.getRawClasspath());
+    assertEquals(1, sources.size());
+    assertEquals(project.getFolder("src/main/aspect").getFullPath(), sources.get(0).getPath());
 
-//    String[] aspectPath = AspectJCorePreferences.getResolvedProjectAspectPath(project);
-//    assertEquals(1, aspectPath.length);
-//    assertTrue(aspectPath[0].endsWith("maven-core-2.1-SNAPSHOT.jar"));
+    String[] aspectPath = AspectJCorePreferences.getResolvedProjectAspectPath(project);
+    assertEquals("The result of getResolvedProjectAspectPath is always an array of length 3", 3, aspectPath.length);
+    assertTrue(aspectPath[0].contains("/maven-core-"));
+    assertEquals("Should be one segment of this aspect path", aspectPath[0].length()-1, aspectPath[0].indexOf(':'));
+    assertTrue("Content kind should be BINARY",aspectPath[1].startsWith("2")); //$NON-NLS-1$ //$NON-NLS-2$
+    assertTrue("Entry kind should be LIBRARY", aspectPath[2].startsWith("1")); //$NON-NLS-1$ //$NON-NLS-2$
   }
 
-//  public void _test_interProject() throws IOException, CoreException {
-//    ResolverConfiguration configuration = new ResolverConfiguration();
-//    IProject[] projects = importProjects("projects", new String[] {"/p01/pom.xml", "/p02/pom.xml"}, configuration);
-//    
-//    String[] aspectPath = AspectJCorePreferences.getResolvedProjectAspectPath(projects[1]);
-//    assertEquals(1, aspectPath.length);
-//    assertTrue(aspectPath[0].endsWith("p01"));
-//  }
+  public void _test_interProject() throws IOException, CoreException {
+    ResolverConfiguration configuration = new ResolverConfiguration();
+    IProject[] projects = importProjects("projects", new String[] {"/p01/pom.xml", "/p02/pom.xml"}, configuration);
+    
+    String[] aspectPath = AspectJCorePreferences.getResolvedProjectAspectPath(projects[1]);
+    assertEquals(1, aspectPath.length);
+    assertTrue(aspectPath[0].endsWith("p01"));
+  }
 
   private List<IClasspathEntry> getSources(IClasspathEntry[] rawCp) {
     ArrayList<IClasspathEntry> sources = new ArrayList<IClasspathEntry>();
